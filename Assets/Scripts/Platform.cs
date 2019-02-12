53e692b07ac6ae7958165f8d0dffc8fc9409ce52
Mehmet Musa <mmusa@sabanciuniv.edu>
2019-02-12 23:25:58 +0300

    12.02.2019

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -9,10 +9,13 @@ Blokları yaratıyor, konumlandırıyor input alarak kaydırıp yanlış input g
 
 public class Platform : MonoBehaviour
 {
+	
+
     private InputManager ınput;
     public GameHandler game;
     private PlatformSizeHandler platformSizeHandler;
     private UIHandler uI;
+	private ExplosionParticleSystem explosionParticleSystem;
 
     public GameObject block; //kırmızı bloklar
     public GameObject runner; //koşan arkadaş artık neyse
@@ -22,10 +25,10 @@ public class Platform : MonoBehaviour
 
     public List<GameObject> platfotmTiles; //blokları barındıran liste
 
-    private float distance; //bi sonraki bloğun gelceği y mesafesi.  habire artıyor
-    public float distBetweenBlock; //bloklar arası x mesafesi
+    private float distance; //bi sonraki bloğun gelceği y mesafesi
+    private float distBetweenBlock;
 
-    public float[] BlockPos; // blokların oluşailceği pozisyonlar
+    public float[] BlockPos;
 
     private int blockNum; // kaç tane blok olcağı
 
@@ -35,12 +38,8 @@ public class Platform : MonoBehaviour
     private int sameLine = 0;
 
     private int point;
-    public int gainedPoint;
 
-    public int pushBlockForward; // sıranın en sonuna atılcak blok. en arkada kalan blok
-
-    public float straightRoadLenght;
-    public float initialStraightRoadLenght; // for refence point of starting size
+    public int pushBlockForward; // sıranın en sonuna atılcak blok
 
     public static Platform instance;
 
@@ -64,6 +63,8 @@ public class Platform : MonoBehaviour
 
         uI = (UIHandler)FindObjectOfType(typeof(UIHandler));
 
+		explosionParticleSystem = (ExplosionParticleSystem)FindObjectOfType (typeof(ExplosionParticleSystem));
+
 		block = GameObject.FindWithTag("Block");
         runner = GameObject.FindWithTag("Runner");
 		lines = GameObject.FindWithTag("Lines");
@@ -79,7 +80,7 @@ public class Platform : MonoBehaviour
         platfotmTiles = new List<GameObject>();
         platfotmTiles.Add(block);
 
-        BlockPos = new float[] {/*-2 * distBetweenBlock,*/-1 * distBetweenBlock, distBetweenBlock/*,2 * distBetweenBlock*/};
+        BlockPos = new float[] {-distBetweenBlock,distBetweenBlock};
         distance = -5f; // Start from -5
 
         int levelStartStraightLine = 7; // start from third block to give full road
@@ -87,7 +88,7 @@ public class Platform : MonoBehaviour
         platfotmTiles[platfotmTiles.Count - 1].transform.position = new Vector2(0f, distance);
 
 
-        for (int i = 0; i < levelStartStraightLine; i++)
+        for (int i = 0; i < levelStartStraightLine;i++)
         {
             distance += distBetweenBlock;
             platfotmTiles.Add((GameObject)Instantiate(block, this.transform));
@@ -95,39 +96,23 @@ public class Platform : MonoBehaviour
 
         }
 
-        blockNum = 20;
+        blockNum = 10;
 
         for (int i = 0; i < blockNum; i++)
         {
             platfotmTiles.Add((GameObject)Instantiate(block, this.transform));
             platfotmTiles[platfotmTiles.Count - 1].transform.position = BlockPositioner(distBetweenBlock);
         }
-
-       /* if (!Data.isAngled)
-        {
-            foreach (GameObject g in platfotmTiles)
-            {
-                g.transform.GetChild(0).gameObject.SetActive(false);
-            }
-        }*/
-
-        runner.transform.position = instance.platfotmTiles[4].transform.position; //Runner starts from 4rd tile
-
-        blockToSlide = levelStartStraightLine + 1;
+        blockToSlide = 0;
         pushBlockForward = 0;
-        initialStraightRoadLenght = platfotmTiles[blockToSlide].transform.position.y - runner.transform.position.y; // camera ve kombo için uzaklık hesapla
-        straightRoadLenght = initialStraightRoadLenght; // camera ve kombo için uzaklık hesapla
-
-        Debug.Log("Initial length is : " + initialStraightRoadLenght);
 
         point = 0;
-        gainedPoint = 1;
     }
 
     //runner bloktan öndeyse bloğu ileri at + lines ı bir ileri taşı
     private void LateUpdate()
     {
-        if(runner.transform.position.y >= platfotmTiles[pushBlockForward].transform.position.y + (10 * distBetweenBlock))
+        if(runner.transform.position.y >= platfotmTiles[pushBlockForward].transform.position.y + (3 * distBetweenBlock))
         {
             lines.transform.position = new Vector2(0f, runner.transform.position.y + 3);
 			road.transform.position = new Vector2(0f, runner.transform.position.y + 3);
@@ -154,17 +139,12 @@ public class Platform : MonoBehaviour
                 MoveTile();
             }
 
-            straightRoadLenght = platfotmTiles[blockToSlide].transform.position.y - runner.transform.position.y; // camera ve kombo için uzaklık hesapla
-
-            //Debug.Log(straightRoadLenght);
-
-            /*if (Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, 0f)) //kaycak bloğa karar veriyor. MoveTile de kayar kaymaz yapılıyor artık
+            if (Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, 0f))
             {
                 blockToSlide = (blockToSlide + 1 < platfotmTiles.Count) ? blockToSlide += 1 : blockToSlide = 0;
-            }*/
+            }
         }
     }
-
 
 
     private void MoveTile()
@@ -173,12 +153,17 @@ public class Platform : MonoBehaviour
         {
             if (ınput.dirr == InputManager.direction.right)
             {
-                if (platfotmTiles[blockToSlide].transform.position.x > 0 )//Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[1])) // if pressed right and next tile is on right
+                if (Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[1])) // if pressed right and next tile is on right
                 {
+					explosionParticleSystem.Explode (platfotmTiles[blockToSlide].transform.position);// xplosion
+					platfotmTiles [blockToSlide].gameObject.SetActive (false); // blow up the block
+
                     StartCoroutine(platfotmTiles[blockToSlide].GetComponent<BlockAnimation>().MoveTile(0));
                     blockToSlide = (blockToSlide + 1 < platfotmTiles.Count) ? blockToSlide += 1 : blockToSlide = 0;
-                    point += gainedPoint;
+                    point++;
                     uI.SetPoint(point);
+
+
                     //platfotmTiles[blockToSlide].transform.position = new Vector2(platfotmTiles[blockToSlide].transform.position.x - distBetweenBlock, platfotmTiles[blockToSlide].transform.position.y);
                 }
                 else // if pressed Right but tile is on left
@@ -191,12 +176,17 @@ public class Platform : MonoBehaviour
             }
             else if (ınput.dirr == InputManager.direction.left)
             {
-                if (platfotmTiles[blockToSlide].transform.position.x < 0 )//Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[0])) // if pressed left and tile is on left
+                if (Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[0])) // if pressed left and tile is on left
                 {
+					explosionParticleSystem.Explode (platfotmTiles[blockToSlide].transform.position); //xplosion
+					platfotmTiles [blockToSlide ].gameObject.SetActive (false); // blow up the block
+
                     StartCoroutine(platfotmTiles[blockToSlide].GetComponent<BlockAnimation>().MoveTile(0));
                     blockToSlide = (blockToSlide + 1 < platfotmTiles.Count) ? blockToSlide += 1 : blockToSlide = 0;
-                    point += gainedPoint;
+                    point++;
                     uI.SetPoint(point);
+
+
                     //platfotmTiles[blockToSlide].transform.position = new Vector2(platfotmTiles[blockToSlide].transform.position.x + distBetweenBlock, platfotmTiles[blockToSlide].transform.position.y);
                 }
                 else // if pressed left but tile is on right
@@ -212,12 +202,17 @@ public class Platform : MonoBehaviour
         {
             if (ınput.dirr == InputManager.direction.right)
             {
-                if (platfotmTiles[blockToSlide].transform.position.x < 0 )//Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[0])) // if pressed right tile is on left
+                if (Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[0])) // if pressed right tile is on left
                 {
+					explosionParticleSystem.Explode (platfotmTiles[blockToSlide].transform.position); //xplosion
+					platfotmTiles [blockToSlide].gameObject.SetActive (false); // blow up the block
+
                     StartCoroutine(platfotmTiles[blockToSlide].GetComponent<BlockAnimation>().MoveTile(0));
                     blockToSlide = (blockToSlide + 1 < platfotmTiles.Count) ? blockToSlide += 1 : blockToSlide = 0;
-                    point += gainedPoint;
+                    point++;
                     uI.SetPoint(point);
+
+
                     //platfotmTiles[blockToSlide].transform.position = new Vector2(platfotmTiles[blockToSlide].transform.position.x + distBetweenBlock, platfotmTiles[blockToSlide].transform.position.y);
                 }
                 else // if pressed right but reverse worng
@@ -228,66 +223,42 @@ public class Platform : MonoBehaviour
                     uI.GameOver();
                 }
             }
-            else if (ınput.dirr == InputManager.direction.left) 
+            else if (ınput.dirr == InputManager.direction.left) // if pressed left tile is on right
             {
-                if (platfotmTiles[blockToSlide].transform.position.x > 0)//Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[1])) // if pressed left tile is on right
+                if (Mathf.Approximately(platfotmTiles[blockToSlide].transform.position.x, BlockPos[1]))
                 {
+					explosionParticleSystem.Explode (platfotmTiles[blockToSlide].transform.position);// xplosion
+					platfotmTiles [blockToSlide].gameObject.SetActive (false); // blow up the block
+
                     StartCoroutine(platfotmTiles[blockToSlide].GetComponent<BlockAnimation>().MoveTile(0));
                     blockToSlide = (blockToSlide + 1 < platfotmTiles.Count) ? blockToSlide += 1 : blockToSlide = 0;
-                    point += gainedPoint;
+                    point++;
                     uI.SetPoint(point);
+
+
                     //platfotmTiles[blockToSlide].transform.position = new Vector2(platfotmTiles[blockToSlide].transform.position.x - distBetweenBlock, platfotmTiles[blockToSlide].transform.position.y);
                 }
                 else // if pressed left but reverse worng
                 {
                     game.GameOver();
                     StartCoroutine(platfotmTiles[blockToSlide].GetComponent<BlockAnimation>().Fall(new Vector2(-1f, 0)));
+
                     uI.GameOver();
                 }
             }
         }
+
     }
 
     // blokları konumlandıran fonksiyon
     private Vector2 BlockPositioner(float rate)
     {
         int tempEx = exRand;
-        exRand = RandomPos.RandomPosition(exRand, sameLine,BlockPos.Length);
+        exRand = RandomPos.RandomPosition(exRand, sameLine);
         sameLine = (tempEx == exRand) ? sameLine += 1 : sameLine = 0;
         distance += rate;
 
         return new Vector2(BlockPos[exRand], distance);
-    }
-
-    public void ChangeMode() //for mode //new Vector3(0f,0f,-10f)new Vector3(0f, -6f, -10f)
-    {
-        if(!Data.isAngled)
-        {
-            foreach(GameObject g in platfotmTiles)
-            {
-            g.transform.GetChild(0).gameObject.SetActive(false);
-            }
-            Camera.main.gameObject.transform.eulerAngles = Vector3.zero;
-            Camera.main.gameObject.GetComponent<CameraMovement>().CalculateOffset(runner.transform.position + new Vector3(0f, 3f, -10f));
-            //Camera.main.gameObject.transform.position = Vector3.zero;
-        }
-        else
-        {
-            foreach (GameObject g in platfotmTiles)
-            {
-                g.transform.GetChild(0).gameObject.SetActive(true);
-            }
-            Camera.main.gameObject.transform.eulerAngles = new Vector3(-30f, 0f, 0f);
-            Camera.main.gameObject.GetComponent<CameraMovement>().CalculateOffset(runner.transform.position + new Vector3(0f, -3f, -10f));
-            //Camera.main.gameObject.transform.position = new Vector3(0f, 6f, 0f);
-        }
-
-    }
-
-
-    public void GiveMessage(float time,string message)
-    {
-        StartCoroutine(uI.GiveInfo(time,message));
     }
 
 }
--- Assets/Scripts/ScreenSizeHandling/CameraSizeHandler.cs
+++ Assets/Scripts/ScreenSizeHandling/CameraSizeHandler.cs
@@ -2,103 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraSizeHandler : SizeHandler {
+public class CameraSizeHandler : MonoBehaviour {
 
-    public IEnumerator DynamicCameraMovement(float Upperlimit,float LowerLimit)
-    {
-        float camSize = Camera.main.fieldOfView;
-
-        //Şimdilik 9.5 la falan başlıyor ilk yolun mesafesi. ondan hardcoded değiştirilcek ama sonra
-        float roadLenghtUpperLimit = Platform.instance.initialStraightRoadLenght + 4f;
-        float roadLengthLowerLimit = Platform.instance.initialStraightRoadLenght - 4f;
-
-        //Debug.Log(Platform.instance.initialStraightRoadLenght);
-        float roadMiddleReferencePoint = Platform.instance.initialStraightRoadLenght;
-
-        //float distanceBtwLowBounds = roadMiddleReferencePoint - roadLengthLowerLimit;
-        //float distanceBtwUpperBounds = roadLenghtUpperLimit - roadMiddleReferencePoint;
-
-        float camDiffLowBounds = camSize - LowerLimit; // 60 - lower cam size
-        float camDiffUpperBounds = Upperlimit - camSize; // upper cam size - 60
-        //Debug.Log(camDiffUpperBounds);
-
-        float camSizeChange = 0f;
-
-        yield return new WaitUntil(() => Platform.instance.game.state == GameHandler.GameState.GameRunning);
-
-        while(Platform.instance.game.state == GameHandler.GameState.GameRunning)
-        {
-            //Debug.Log(Platform.instance.straightRoadLenght);
-            if ((Platform.instance.straightRoadLenght) > roadMiddleReferencePoint + 1f) //genişlicekse
-            {
-                //Debug.Log("genişlicek bu kadar : " + (camDiffUpperBounds * ((Platform.instance.straightRoadLenght - roadMiddleReferencePoint) / (roadLenghtUpperLimit - roadMiddleReferencePoint))));
-
-                // ne kadar genişlemesi gerektiği
-                float newCamSize  = camSize + (camDiffUpperBounds * ((Platform.instance.straightRoadLenght - roadMiddleReferencePoint) / (roadLenghtUpperLimit - roadMiddleReferencePoint)));
-
-                //ona doğru git
-                if(Camera.main.fieldOfView <  newCamSize)
-                {
-                    camSizeChange = ChangeSizeTo(0.1f, camSizeChange);
-                    //Camera.main.fieldOfView += camSizeChange;
-                    Camera.main.fieldOfView += 0.1f;
-                }
-                else if(Camera.main.fieldOfView > newCamSize + 0.5f)
-                {
-                    camSizeChange = ChangeSizeTo(-0.1f, camSizeChange);
-                    //Camera.main.fieldOfView += camSizeChange;
-                    Camera.main.fieldOfView -= 0.1f;
-                }
-
-                //Camera.main.fieldOfView = camSize + (camDiffUpperBounds * ((Platform.instance.straightRoadLenght - roadMiddleReferencePoint) / (roadLenghtUpperLimit - roadMiddleReferencePoint)));
-            }
-            else if (Platform.instance.straightRoadLenght <= roadMiddleReferencePoint - 1f) // daralcaksa
-            {
-                //Debug.Log("daralcak  bu kadar : " + (camDiffLowBounds * ((roadMiddleReferencePoint - Platform.instance.straightRoadLenght) / (roadMiddleReferencePoint - roadLengthLowerLimit))));
-
-                // ne kadar daralması gerektiği
-                float newCamSize = camSize - (camDiffLowBounds * ((roadMiddleReferencePoint - Platform.instance.straightRoadLenght) / (roadMiddleReferencePoint - roadLengthLowerLimit)));
-
-                //ona doğru git
-                if(Camera.main.fieldOfView > newCamSize)
-                {
-                    camSizeChange = ChangeSizeTo(-0.1f, camSizeChange);
-                    //Camera.main.fieldOfView += camSizeChange;
-                    Camera.main.fieldOfView -= 0.1f;
-                }
-                else if(Camera.main.fieldOfView < newCamSize -0.5f)
-                {
-                    camSizeChange = ChangeSizeTo(0.1f, camSizeChange);
-                    //Camera.main.fieldOfView += camSizeChange;
-                    Camera.main.fieldOfView += 0.1f;
-                }
-
-                //Camera.main.fieldOfView = camSize - (camDiffLowBounds * ((roadMiddleReferencePoint - Platform.instance.straightRoadLenght) / (roadMiddleReferencePoint - roadLengthLowerLimit)));
-            }
-            /*else // düz 60
-            {
-                Camera.main.fieldOfView = camSize;
-            }*/
-
-            yield return new WaitForSeconds(0.001f);
-        }
-    }
-
-    // change camera size forward to
-        float ChangeSizeTo(float direction, float changeSize)
-        {
-
-
-            if (direction > 0)
-            {
-                changeSize = (changeSize < direction) ? changeSize + 0.01f : changeSize = .01f;
-            }
-            else if(direction < 0)
-            {
-                changeSize = (changeSize > direction) ? changeSize - 0.01f : changeSize = -0.1f;
-            }
-            
-            return changeSize;
-        } 
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
