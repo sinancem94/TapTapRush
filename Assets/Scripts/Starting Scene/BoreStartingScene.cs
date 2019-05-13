cde864c82be5697769a4a81eb5aa5f4ab16b4023
Mehmet Musa <mmusa@sabanciuniv.edu>
2019-05-13 23:45:50 +0300

    13.05.2019

---
+++ Assets/Scripts/Starting Scene/BoreStartingScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoreStartingScene : MonoBehaviour
+{
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position =    Camera.main.WorldToScreenPoint(image.transform.position) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
